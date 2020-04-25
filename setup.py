29f78de387ce79085994461273078ee259f8c2b0
Adan Luis Benito Temprano <alb30@buxton.eecs.qmul.ac.uk>
2020-04-25 09:18:23 +0100

    Project template

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup, find_packages
+
+setup(
+	name='dl4am',
+	version='0.0dev',
+	packages=find_packages(),
+	author='Adan L. Benito',
+	author_email='a.benitotemprano@qmul.ac.uk',
+	license='MIT'
+)
+
