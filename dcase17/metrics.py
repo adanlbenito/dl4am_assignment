a418a29ad15017cca63f5131415a30e9b2dafbda
Adan Luis Benito Temprano <alb30@buxton.eecs.qmul.ac.uk>
2020-04-30 06:04:38 +0100

    Added utilities to metrics

--- dcase17/metrics.py
+++ dcase17/metrics.py
@@ -1,5 +1,11 @@
 import numpy as np
-import dcase17.utils
+#####################
+# Utils
+#####################
+def reshape_3Dto2D(A):
+    return A.reshape(A.shape[0] * A.shape[1], A.shape[2])
+
+eps = np.finfo(np.float).eps
 #####################
 # Scoring functions
 #
@@ -13,19 +19,19 @@ import dcase17.utils
 
 def f1_overall_framewise(O, T):
     if len(O.shape) == 3:
-        O, T = utils.reshape_3Dto2D(O), utils.reshape_3Dto2D(T)
+        O, T = reshape_3Dto2D(O), reshape_3Dto2D(T)
     TP = ((2 * T - O) == 1).sum()
     Nref, Nsys = T.sum(), O.sum()
 
-    prec = float(TP) / float(Nsys + utils.eps)
-    recall = float(TP) / float(Nref + utils.eps)
-    f1_score = 2 * prec * recall / (prec + recall + utils.eps)
+    prec = float(TP) / float(Nsys + eps)
+    recall = float(TP) / float(Nref + eps)
+    f1_score = 2 * prec * recall / (prec + recall + eps)
     return f1_score
 
 
 def er_overall_framewise(O, T):
     if len(O.shape) == 3:
-        O, T = utils.reshape_3Dto2D(O), utils.reshape_3Dto2D(T)
+        O, T = reshape_3Dto2D(O), reshape_3Dto2D(T)
     FP = np.logical_and(T == 0, O == 1).sum(1)
     FN = np.logical_and(T == 1, O == 0).sum(1)
 
@@ -40,7 +46,7 @@ def er_overall_framewise(O, T):
 
 def f1_overall_1sec(O, T, block_size):
     if len(O.shape) == 3:
-        O, T = utils.reshape_3Dto2D(O), utils.reshape_3Dto2D(T)
+        O, T = reshape_3Dto2D(O), reshape_3Dto2D(T)
     new_size = int(np.ceil(O.shape[0] / block_size))
     O_block = np.zeros((new_size, O.shape[1]))
     T_block = np.zeros((new_size, O.shape[1]))
@@ -52,7 +58,7 @@ def f1_overall_1sec(O, T, block_size):
 
 def er_overall_1sec(O, T, block_size):
     if len(O.shape) == 3:
-        O, T = utils.reshape_3Dto2D(O), utils.reshape_3Dto2D(T)
+        O, T = reshape_3Dto2D(O), reshape_3Dto2D(T)
     new_size = int(O.shape[0] / block_size)
     O_block = np.zeros((new_size, O.shape[1]))
     T_block = np.zeros((new_size, O.shape[1]))
